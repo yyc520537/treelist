daeca79c5ad52f863c81cb59ac38838c39fdf13e
YANGYANCHENG <YANGYANCHENG@SOFTING-0032>
2024-04-23 10:04:46 +0800

    添加项目文件。

---
+++ treelist/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿using DevExpress.XtraTreeList;
+using System.Data;
+
+namespace treelist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.treeList1 = new DevExpress.XtraTreeList.TreeList();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.LoadXmlButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.treeList1);
+            this.panelControl1.Location = new System.Drawing.Point(12, 12);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(439, 371);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // treeList1
+            // 
+            this.treeList1.Location = new System.Drawing.Point(0, 5);
+            this.treeList1.Name = "treeList1";
+            this.treeList1.OptionsBehavior.AllowRecursiveNodeChecking = true;
+            this.treeList1.OptionsCustomization.AllowColumnMoving = false;
+            this.treeList1.OptionsView.CheckBoxStyle = DevExpress.XtraTreeList.DefaultNodeCheckBoxStyle.Check;
+            this.treeList1.Size = new System.Drawing.Size(439, 361);
+            this.treeList1.TabIndex = 0;
+            this.treeList1.FocusedNodeChanged += new DevExpress.XtraTreeList.FocusedNodeChangedEventHandler(this.treeList1_FocusedNodeChanged);
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(this.LoadXmlButton);
+            this.panelControl2.Location = new System.Drawing.Point(12, 389);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(439, 43);
+            this.panelControl2.TabIndex = 1;
+            // 
+            // LoadXmlButton
+            // 
+            this.LoadXmlButton.Location = new System.Drawing.Point(5, 0);
+            this.LoadXmlButton.Name = "LoadXmlButton";
+            this.LoadXmlButton.Size = new System.Drawing.Size(429, 43);
+            this.LoadXmlButton.TabIndex = 0;
+            this.LoadXmlButton.Text = "选择XML文件";
+            this.LoadXmlButton.UseVisualStyleBackColor = true;
+            this.LoadXmlButton.Click += new System.EventHandler(this.LoadXmlButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 434);
+            this.Controls.Add(this.panelControl2);
+            this.Controls.Add(this.panelControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "XML文件结构解析";
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraTreeList.TreeList treeList1;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private System.Windows.Forms.Button LoadXmlButton;
+    }
+}
+
---
+++ treelist/Form1.cs
@@ -0,0 +1,97 @@
+﻿using DevExpress.XtraEditors.Filtering;
+using DevExpress.XtraTreeList.Columns;
+using DevExpress.XtraTreeList.Nodes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace treelist
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        private int nodeId = 0; // 用于生成唯一的节点ID
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTreeList();
+        }
+
+        private void treeList1_FocusedNodeChanged(object sender, DevExpress.XtraTreeList.FocusedNodeChangedEventArgs e)
+        {
+            // 可以在这里添加逻辑来处理节点焦点改变事件
+            if (e.Node != null)
+            {
+                Console.WriteLine("Selected Node: " + e.Node.GetValue("Name"));
+            }
+        }
+
+        private void InitializeTreeList()
+        {
+            treeList1.Columns.Add(new TreeListColumn { Caption = "Name", FieldName = "Name", VisibleIndex = 0 });
+            treeList1.Columns.Add(new TreeListColumn { Caption = "id", FieldName = "id", VisibleIndex = 1 });
+            treeList1.KeyFieldName = "ID";
+            treeList1.ParentFieldName = "ParentID";
+        }
+
+        //点击按钮，选择XML文件地址
+        private void LoadXmlButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All files (*.*)|*.*";//XML files (*.xml)|*.xml|
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                LoadXmlData(openFileDialog.FileName);//读取文件
+            }
+        }
+        private void LoadXmlData(string filePath)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.Load(filePath);
+                XmlToTree(xmlDoc.DocumentElement, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("XML文件读取错误: " + ex.Message);
+            }
+        }
+
+        private void XmlToTree(XmlNode xmlNode, TreeListNode parentNode)
+        {
+            // 初始化节点名称为XML节点的标签名。
+            string nodeName = xmlNode.Name;
+            // 检查是否存在name属性，并且该属性是否有值。
+            XmlAttribute nameAttribute = xmlNode.Attributes?["name"];
+            if (nameAttribute != null && !string.IsNullOrEmpty(nameAttribute.Value))
+            {
+                nodeName = nameAttribute.Value; // 使用name属性的值作为节点名称。
+            }
+            string Id = "";
+            XmlAttribute id = xmlNode.Attributes?["id"];
+            if (id != null && !string.IsNullOrEmpty(id.Value))
+            {
+                Id = id.Value; // 使用name属性的值作为节点名称。
+            }
+
+            // 创建一个新的树节点，往treelist添加数据
+            TreeListNode node = treeList1.AppendNode(new object[] { nodeName, Id }, parentNode);
+            // 递归遍历子节点。
+            foreach (XmlNode childNode in xmlNode.ChildNodes)
+            {
+                // 只处理元素类型的节点。
+                if (childNode.NodeType == XmlNodeType.Element)
+                {
+                    XmlToTree(childNode, node);
+                }
+            }
+        }
+    }
+}
